3d2b3376650168a9b983a89195eefd64502dc675
Thays Casado <thayscasado@gmail.com>
2024-04-01 18:32:52 -0700

    start project with vite and tailwind

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+
+import nlwUnitedIcon from '../assets/nwl-united-icon.svg';
+
+export function Header() {
+  return (
+    <div className='flex items-center gap-5 py-2'>
+    <img src={nlwUnitedIcon} alt="" />
+    <nav className='flex items-center gap-5'>
+        <a className="font-medium text-sm text-zinc-300" href="">Events</a>
+        <a className="font-medium text-sm" href="">Attendees</a>
+    </nav>
+    </div>
+  );
+}
