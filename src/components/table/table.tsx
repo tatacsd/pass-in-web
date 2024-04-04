109f84f478aff6a42444a67c549297483c8c0c73
Thays Casado <thayscasado@gmail.com>
2024-04-03 20:22:52 -0700

    ♻️ add table component
    using component props and spread operator

---
+++ src/components/table/table.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from "react";
+
+interface TableProps extends ComponentProps<"table"> {}
+
+export function Table(props: TableProps){
+    return (
+        <div className="rounded-lg border border-white/10">
+        <table className="w-full" {...props}/>
+        </div>
+    )
+
+}
