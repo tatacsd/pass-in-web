c9f9f2149195c82046f6f3ad0b57f262ea81b2fb
Thays Casado <thayscasado@gmail.com>
2024-04-03 19:59:15 -0700

    ♻️ create navlink component
    applied ComponentProps and children

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
-
-import nlwUnitedIcon from '../assets/nwl-united-icon.svg';
+import nlwUnitedIcon from "../assets/nwl-united-icon.svg";
+import { NavLink } from "./nav-link";
 
 export function Header() {
   return (
-    <div className='flex items-center gap-5 py-2'>
-    <img src={nlwUnitedIcon} alt="" />
-    <nav className='flex items-center gap-5'>
-        <a className="font-medium text-sm text-zinc-300" href="">Events</a>
-        <a className="font-medium text-sm" href="">Attendees</a>
-    </nav>
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUnitedIcon} alt="" />
+      <nav className="flex items-center gap-5">
+        <NavLink href="/events">Events</NavLink>
+        <NavLink href="/participants">Participants</NavLink>
+      </nav>
     </div>
   );
 }
